7fd83cfea0511b5c554b8d9f4a9719b363703d54
Vivek Bhatt <vivek.bhatt@devdigital.com>
2020-08-21 19:36:14 +0530

    version change

--- src/test/AddBugTask.java
+++ src/test/AddBugTask.java
@@ -63,7 +63,7 @@ public class AddBugTask extends Utilities {
 	Cell DevTrackerURL;
 	Cell taskType;
 	String DevTrackerStageURL;
-	String version = "v1.1";
+	String version = "v1.2";
 	String error = "";
 	String headless;
 	File file;
